bcfd8c637b050ad975183002ec8e0bb30e5011b4
Danil Kolumbet <kolumbetko@gmail.com>
2021-03-16 21:58:47 +0200

    Add asteroid surface generation

--- asteroid.js
+++ asteroid.js
@@ -5,7 +5,20 @@ class Asteroid {
         this.angleSpeed = angleSpeed;
         this.size = size;
         this.vel = vel;
-        this.points = [new Vector(20, 20), new Vector(-20, 20), new Vector(-20, -20), new Vector(20, -20)];
+        this.points = [];
+        const numberOfPoints = size * 0.75;
+        const radiusCore = 1;
+        const radiusNoise = 18 / size;
+        let lastAngle = 0;
+        for (let i = 0; i < numberOfPoints; i++) {
+            const angleStep = (Math.random() * 1.6 + 0.2) * (Math.PI * 2 - lastAngle) / (numberOfPoints - i);
+            const angle = clamp(lastAngle + angleStep, Math.PI * 2);
+            lastAngle = angle;
+            const radius = size * radiusCore + Math.random() * size * radiusNoise;
+            const x = radius * Math.cos(angle);
+            const y = radius * Math.sin(angle);
+            this.points.push(new Vector(x, y));
+        }
     }
     update () {
         this.pos.addMut(this.vel);
